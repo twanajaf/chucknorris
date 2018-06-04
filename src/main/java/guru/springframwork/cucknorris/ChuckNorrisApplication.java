3c3deddc5db30ed1004d01626221a2669a0fc9f4
twana <twja@chess-ix.com>
2018-06-04 12:30:58 +0200

    replaced java configuration with xml configuration

--- src/main/java/guru/springframwork/cucknorris/ChuckNorrisApplication.java
+++ src/main/java/guru/springframwork/cucknorris/ChuckNorrisApplication.java
@@ -1,11 +1,11 @@
 package guru.springframwork.cucknorris;
 
-import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class ChuckNorrisApplication {
 
 	public static void main(String[] args) {
